ec20d65c7b1fc4df446ebd15fe3c5965e94c040b
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2024-04-04 01:32:06 +0500

    34th day of code

---
+++ Day 34/100_javascrpt_math_object/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+const squareRoot = Math.sqrt(144);
+console.log(`The square root of 144 is ${squareRoot}`);
