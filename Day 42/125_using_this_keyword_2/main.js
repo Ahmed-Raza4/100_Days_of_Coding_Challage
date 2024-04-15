a2c0f7cdc0cb06fca6a2ced7f7361330ffa51280
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2024-04-16 00:01:05 +0500

    42 day of  code

---
+++ Day 42/125_using_this_keyword_2/main.js
@@ -0,0 +1,17 @@
+"use strict";
+/*Question 125: Modify a method in an object
+to use the this keyword to access another
+property in the same object.
+
+Explain & TIP: You can leverage the this keyword
+to interact with multiple properties within
+the same object, providing a cohesive way
+to manipulate internal data.*/
+let addition = {
+    firstNumber: 20,
+    secondNumber: 30,
+    add: function () {
+        return this.firstNumber + this.secondNumber;
+    }
+};
+console.log(addition.add());
---
+++ Day 42/126_this_keyword_change_value/main.js
@@ -0,0 +1,23 @@
+"use strict";
+/*Question 126: Explain how the this keyword
+changes its value when used inside a nested
+function within a method.
+
+Explain & TIP: When this is used inside a
+nested function within an object's method,
+it does not refer to the object itself but
+to the global object (in a browser, window).
+This behavior often requires workarounds like
+assigning this to another variable in the
+outer function.*/
+const object = {
+    property: "value",
+    outerMethod: function () {
+        console.log(this.property);
+        const innerMethod = () => {
+            console.log(this.property);
+        };
+        innerMethod();
+    }
+};
+object.outerMethod();
